2a93d82f628410ad4f8afa1626a23686c8014b35
Vladimir Alyamkin <ufna@ufna.ru>
2014-06-21 22:41:32 +0400

    Character core class

---
+++ Source/Krieger/Classes/Player/KriegerCharacter.h
@@ -0,0 +1,12 @@
+// Copyright 2014 Vladimir Alyamkin. All Rights Reserved.
+
+#include "KriegerCharacter.generated.h"
+
+UCLASS(Abstract)
+class AKriegerCharacter : public ACharacter
+{
+	GENERATED_UCLASS_BODY()
+
+
+};
+
---
+++ Source/Krieger/Private/Player/KriegerCharacter.cpp
@@ -0,0 +1,9 @@
+// Copyright 2014 Vladimir Alyamkin. All Rights Reserved.
+
+#include "Krieger.h"
+
+AKriegerCharacter::AKriegerCharacter(const class FPostConstructInitializeProperties& PCIP) 
+	: Super(PCIP)
+{
+	
+}
