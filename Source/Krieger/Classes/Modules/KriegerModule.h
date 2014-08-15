c99bd2c356f5862525215fa44208e5ae0a5e54b0
Vladimir Alyamkin <ufna@ufna.ru>
2014-08-15 06:17:31 +0400

    Simple module class to be base class for all visual modules with active effects

---
+++ Source/Krieger/Classes/Modules/KriegerModule.h
@@ -0,0 +1,14 @@
+// Copyright 2014 Vladimir Alyamkin. All Rights Reserved.
+
+#include "KriegerModule.generated.h"
+
+/**
+ * Common class for special modules like jetpack. Modules have fuel aka ammo, can be activated, etc.
+ */
+UCLASS(Abstract, Blueprintable)
+class AKriegerModule : public AKriegerWeapon
+{
+	GENERATED_UCLASS_BODY()
+
+
+};
---
+++ Source/Krieger/Private/Modules/KriegerModule.cpp
@@ -0,0 +1,8 @@
+// Copyright 2014 Vladimir Alyamkin. All Rights Reserved.
+
+#include "Krieger.h"
+
+AKriegerModule::AKriegerModule(const class FPostConstructInitializeProperties& PCIP) : Super(PCIP)
+{
+	
+}
